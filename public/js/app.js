3ff48d5e3a43b314f2e75c90bf94abb04898e2f5
Juliana <olartezapata14@gmail.com>
2025-11-05 09:44:22 -0500

    first commit

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,6 @@
 import { OpenStreetMapProvider } from "leaflet-geosearch";
+import * as Esri from 'esri-leaflet';
+import * as Geocoder from 'esri-leaflet-geocoder';
 
 const lat = 4.81428
 const lng = -75.69488
@@ -6,10 +8,8 @@ const map = L.map('mapa').setView([lat, lng], 15); //15 --> zoom
 let markers = new L.FeatureGroup().addTo(map);
 let marker;
 
-//utilizar provider y GeoCoder
-const geocodeService = L.esri.Geocoding.geocodeService();
-
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {   
+    
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map); 
@@ -19,50 +19,45 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 function buscarDireccion(e) {
-    if(e.target.value.length > 8){
-
-        //si existe un marker anterior, eliminarlo
+    if(e.target.value.length > 8) {
+        
+        // si existe un pin anterior limpiarlo
         markers.clearLayers();
 
+        const geocodeService = Geocoder.geocodeService();
         const provider = new OpenStreetMapProvider();
         provider.search({ query: e.target.value }).then(( resultado ) => {
 
             geocodeService.reverse().latlng(resultado[0].bounds[0], 15).run(function(error, result) {
-
+                //quité zoom de 15
+                //el console.log da undefined
                 console.log(result);
                 
-                //mostrar el mapa
+                // console.log(resultado);
+                // mostrar el mapa
                 map.setView(resultado[0].bounds[0], 15);
-                
-                //agregar pin
+
+                // agregar el pin
                 marker = new L.marker(resultado[0].bounds[0], {
-                    draggable: true,
+                    draggable : true,
                     autoPan: true
-                
                 })
                 .addTo(map)
-                .bindPopup(resultado[0].label)     
-                .openPopup();       
+                .bindPopup(resultado[0].label)
+                .openPopup();
 
-                //asignar al contenedor
+                // asignar al contenedor markers
                 markers.addLayer(marker);
 
-                //Detectar movimiento del marker
-                marker.on('moveend', function(e){
+                // detectar movimiento del marker
+                marker.on('moveend', function(e) {
                     marker = e.target;
                     const posicion = marker.getLatLng();
-                    map.panTo(new L.LatLng(posicion.lat, posicion.lng)); //panTo --> centra el mapa en la nueva posicion 
+                    map.panTo(new L.LatLng(posicion.lat, posicion.lng) );                
                     
-                    //reverse geocoding, cuando el usuario reubica el marker
-                    geocodeService.reverse().latlng(posicion, 15).run(function(error, result) {
+                })
+            })
 
-                        console.log(result);
-                    })
-               
-                });
-
-            });
-
-        });
+        })
     }
-}    
+}
